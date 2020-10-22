f5948efbf77fcd114ec1138d578eeb56db727ffc
HarlyquinForest <aminyousefnejad28@gmail.com>
2020-10-22 15:34:11 +0330

    initial commit of project

---
+++ colors.py
@@ -0,0 +1,17 @@
+class Colors:
+    BLACK  = "\033[0;30m"
+    RED    = "\033[0;31m"
+    GREEN  = "\033[0;32m"
+    BROWN  = "\033[0;33m"
+    BLUE   = "\033[0;34m"
+    PURPLE = "\033[0;35m"
+    CYAN   = "\033[0;36m"
+    YELLOW = "\033[1;33m"
+    BOLD   = "\033[1m"
+    FAINT  = "\033[2m"
+    ITALIC = "\033[3m"
+    UNDERLINE = "\033[4m"
+    BLINK  = "\033[5m"
+    NEGATIVE  = "\033[7m"
+    CROSSED  = '\033[9m'
+    END = "\033[0m"
---
+++ note.py
@@ -0,0 +1,213 @@
+
+from pathlib import Path
+import datetime 
+import os
+import sys , getopt
+from colors import Colors
+import argparse
+HOMEDIR = str(Path.home())
+TODAY = datetime.datetime.now().strftime('%x')
+COMMAND = ['add' , 'list' , 'done' , 'alldone']
+TODAYNOTE = f'{HOMEDIR}/today'
+TODAYDONE = f'{HOMEDIR}/done'
+DONE = []
+
+
+def file_validation() : 
+
+    status = int(read_date())
+    if status == 1 :
+        create_today()
+
+    global COUNTER 
+    COUNTER = counter()
+
+def counter(inc=True):
+
+    if os.path.exists('/tmp/note_counter') :
+        f = open('/tmp/note_counter' , 'r')
+        t = f.readline()
+        f.close()
+        if inc : 
+            c = int(str.rstrip(t)) + 1
+            f = open('/tmp/note_counter' , 'w')
+            f.write(str(c))
+            f.close()
+            return  c 
+        else :
+            return int(str.rstrip(t))
+    else :
+        f = open('/tmp/note_counter' , 'w')
+        f.write(str(1))
+        f.close()
+        return 1 
+
+def done_items(): 
+    file = open(TODAYDONE , 'r')
+    while True : 
+        num = file.readline()
+        if num == '':
+            break 
+        DONE.append(int(num))
+    file.close()
+
+def create_today():
+    file = open(TODAYNOTE , 'w')
+    file.write(TODAY+"\n")
+    file.close()
+
+def write_file(text):
+    file = open(TODAYNOTE , 'a')
+    file.write(str(COUNTER)+" "+text+"\n")
+    file.close()
+    print("OK")
+
+def read_file(all=True , complete = False , undone = False):
+    done_items()
+    file = open(TODAYNOTE , 'r')
+    today_header = f'\n——— {TODAY} ———\n'
+    print(today_header)
+    total = 0 
+    counter = 0
+    file.seek(9)
+    while(1):
+        line = file.readline()
+        if( line == ''):
+            break;
+        counter +=1 
+        if counter in DONE and (complete == True or all == True )  : 
+            print(Colors.CROSSED+" "+line + Colors.END , end="")
+        elif all == True or undone == True and counter not in DONE: 
+            print(" "+line,end="")
+        total +=1 
+    today_footer = f'\n——— total {total} ———\n'
+    print(today_footer)
+    file.close()
+
+def done(nums):
+    print(f'{nums} marked as done.')
+    file = open(TODAYDONE , 'a')
+    for num in nums:
+        file.writelines(num+"\n")
+    file.close()
+
+def undone(nums):
+    file = open(TODAYDONE , 'r')
+    lines = file.readlines()
+    file.close()
+    for num in nums :
+        try:
+            index = lines.index(nums[0]+'\n')
+        except ValueError : 
+            print("selected item is not marked as completed")
+        del(lines[index])
+        print(f'{num} is undone now')
+
+    file = open(TODAYDONE , 'w')
+    file.writelines(lines)
+    file.close()
+
+def read_date():
+    try:
+        file = open(TODAYNOTE , 'r')
+    except FileNotFoundError :
+        return 1 
+
+    line = file.readline()
+    file.close()
+
+    if TODAY in  line : 
+        return 0 
+    else :
+        return 1
+
+def remove_items(nums):
+    file = open(TODAYNOTE , 'r')
+    lines = file.readlines()
+    file.close()
+
+    for num in nums : 
+        num = int(num)
+        if int(num) > len(lines) -1 :
+            num -= (int(num) - (len(lines) - 1) )
+        if num != 0 : 
+            del(lines[num])
+            print(f"item {num} deleted")
+        else: 
+            print(f"There is no {num} index ")
+    file = open(TODAYNOTE , 'w')
+    file.writelines(lines)
+    file.close()
+    
+def main(argv):
+
+    #
+    # command line arguments 
+    #
+    parser = argparse.ArgumentParser(description='Create to-do and shopping list' , usage='''note <command> [<args>]''')
+    parser.add_argument('-n' , help='create new item in today list' , nargs='+')
+    parser.add_argument('-l' , help='list all the items in today list' , nargs='?' , default='0' ,  const='1')
+    parser.add_argument('-d' , help='mark selected items from today list done ' , nargs='+')
+    parser.add_argument('-a' , help='using with -l make the list to present all the items of todo list . Default mode for -l option' , action='store_true' , default=True)
+    parser.add_argument('-c' , help="using with -l make the list to present only completed items " , action='store_true')
+    parser.add_argument('-u' , help='using with -l make the list to present only uncompleted items' , action='store_true')
+    parser.add_argument('-r' , help='removes the items form todo list' , nargs='+')
+    parser.add_argument('-x' , help='make selected items mark as undone if its make done ' , nargs='+' )
+
+    #
+    # parsing arguments 
+    #
+    args = parser.parse_args()
+    new_arg = args.n 
+    list_arg = args.l 
+    done_arg = args.d 
+    undone_arg = args.x
+    remove_arg = args.r
+
+    #
+    # each argument instructions 
+    #
+    if new_arg is not None : 
+        message = ' '.join(new_arg)
+        file_validation()
+        write_file(message)
+    
+    if '1' in list_arg : 
+        if args.c is True or args.u is True : 
+            args.a = False 
+        read_file(all=args.a , complete=args.c , undone=args.u)
+
+    elif 'u' in list_arg or 'a' in list_arg or 'c' in list_arg :
+        list_arg = split(list_arg)
+        a = False 
+        c = False 
+        u = False 
+        if 'a' in list_arg: 
+            a = True 
+        elif 'c' in list_arg:
+            c = True 
+        elif 'u' in list_arg:
+            u = True
+
+        read_file(all=a , complete=c , undone=u)
+   
+    if done_arg is not None : 
+        done(done_arg)
+    
+    if undone_arg is not None : 
+        undone(undone_arg)
+        read_file()
+
+    if remove_arg is not None : 
+        remove_items(remove_arg)
+
+    
+
+
+
+def split(word):
+    return [char for char in word]
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
