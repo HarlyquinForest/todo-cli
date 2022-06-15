0aaa12a7b053ae54a089a9626019bf6f3e0e4afb
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-06-15 11:56:00 +0430

    new command list added to show all the checklists

--- commands.py
+++ commands.py
@@ -6,6 +6,7 @@ class Command(Enum):
     DEL = 3  
     REM = 4 
     DON = 5 
+    LIS = 6 
 
     def recognizeCommand(c):
         if c == 'new':
@@ -18,6 +19,8 @@ class Command(Enum):
             return Command.REM
         elif c == 'don':
             return Command.DON
+        elif c == 'ls':
+            return Command.LIS
         else: 
             return None
 
